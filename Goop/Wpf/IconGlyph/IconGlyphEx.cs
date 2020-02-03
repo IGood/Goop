0426992838b55d2369e1716fe7afc9ce27836c1e
IGood <Ian.M.Good@gmail.com>
2020-02-02 21:34:23 -0800

    added IconGlyph stuff
    also updated to 3.1

---
+++ Goop/Wpf/IconGlyph/IconGlyphEx.cs
@@ -0,0 +1,7 @@
+﻿namespace Goop.Wpf.IconGlyph
+{
+	public static class IconGlyphEx
+	{
+		public static string ToUnicode(this IconGlyph value) => ((char)value).ToString();
+	}
+}
---
+++ Goop/Wpf/IconGlyph/IconGlyphTextBlock.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Goop.Wpf.IconGlyph
+{
+	using DP = DependencyPropertyUtilities<IconGlyphTextBlock>;
+
+	[ContentProperty(nameof(IconGlyph))]
+	public class IconGlyphTextBlock : TextBlock
+	{
+		static IconGlyphTextBlock()
+		{
+			DP.OverrideMetadata(
+				TextBlock.FontFamilyProperty,
+				new FrameworkPropertyMetadata(new FontFamily("Segoe MDL2 Assets")) { Inherits = false });
+
+			DP.OverrideMetadata(TextBlock.FontSizeProperty, new FrameworkPropertyMetadata(16.0));
+		}
+
+		public static readonly DependencyProperty IconGlyphProperty = DP.Register(_ => _.IconGlyph, IconGlyph.Unknown, IconGlyphPropertyChanged);
+
+		public IconGlyph IconGlyph
+		{
+			get => (IconGlyph)this.GetValue(IconGlyphProperty);
+			set => this.SetValue(IconGlyphProperty, value);
+		}
+
+		private static void IconGlyphPropertyChanged(IconGlyphTextBlock self, DependencyPropertyChangedEventArgs e)
+		{
+			self.Text = ((IconGlyph)e.NewValue).ToUnicode();
+		}
+	}
+}
