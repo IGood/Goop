afbc9ca82797f17997251b38d07b4f5c31915112
IGood <Ian.M.Good@gmail.com>
2020-12-29 01:57:15 -0800

    added "tinker" project - displays all icon glyphs for funsies

---
+++ tinker/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Goop.Wpf.IconGlyph;
+using System;
+using System.Windows;
+
+namespace tinker
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+
+			this.ic.ItemsSource = Enum.GetValues(typeof(IconGlyph));
+		}
+	}
+}
